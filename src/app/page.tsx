aa23806dbde691cca694cb38b04848bc88ad26c1
Riad <riadk910@gmail.com>
2025-05-11 14:33:01 +0600

    Navbar added from accertinity ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-2xl text-center ">Chai Aur Code</h1>;
+  return <h1 className="text-2xl text-center "> Chai Aur Code</h1>;
 }
